ff5d8661cf86d83b9eac005b54571892593a5f74
William Penagos <billalpeza@gmail.com>
2020-11-04 12:42:05 -0500

    fix(remove skipe): from generate testing

--- test/generate-log.spec.js
+++ test/generate-log.spec.js
@@ -6,7 +6,7 @@ const generateLog = require('../src/generate-log');
 const pathFile = path.resolve(__dirname, '../src', 'logfile.log');
 
 describe('Testing generateLog file', function () {
-  it.skip('generateLog', () => {
+  it('generateLog', () => {
     this.timeout(1e4);
     return generateLog(1)
       .then((response) => {
